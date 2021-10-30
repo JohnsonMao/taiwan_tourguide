c5571e515d81f71cf7ba9a765aab7642567c20f4
JohnsonMao <86179381+JohnsonMao@users.noreply.github.com>
2021-10-30 20:17:59 +0800

    add popular cities

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,60 +1,71 @@
-import React from 'react';
-import { Container, Row, Col, Card, Ratio } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col, Card, Ratio } from "react-bootstrap";
 
-import Banner from '../../components/Banner';
-import ActivityList from '../../components/ActivityList';
-import CardList from '../../components/CardList';
-import { ReactComponent as Triangle } from '../../asset/icon/triangle.svg';
+import Banner from "../../components/Banner";
+import ActivityList from "../../components/ActivityList";
+import CardList from "../../components/CardList";
+import { ReactComponent as Triangle } from "../../asset/icon/triangle.svg";
+import { ReactComponent as MapM } from "../../asset/icon/map_M.svg";
+import './static/_popularCities.scss';
 
 function Location() {
+
+  const popularCities = [
+    {city: '台東', mobile: true, img: ''},
+    {city: '台北', mobile: true, img: ''},
+    {city: '台中', mobile: true, img: ''},
+    {city: '台南', mobile: true, img: ''},
+    {city: '高雄', mobile: true, img: ''},
+    {city: '花蓮', mobile: true, img: ''},
+    {city: '屏東', mobile: false, img: ''},
+    {city: '嘉義', mobile: false, img: ''},
+    {city: '南投', mobile: false, img: ''},
+    {city: '宜蘭', mobile: false, img: ''},
+    {city: '苗栗', mobile: false, img: ''},
+    {city: '新竹', mobile: false, img: ''},
+    {city: '外島', mobile: false, img: ''}
+  ]
+
   return (
     <>
       <h4 className="fw-normal fs-2 mb-3">
-        <Triangle className="me-4"/>熱門城市
+        <Triangle className="me-4" />
+        熱門城市
       </h4>
-      <Row lg={5} className="gx-3 section">
-        <Col>
-            <Card className="shadow p-3">
-                <Ratio aspectRatio="3x4">
-                  <img src={require("./static/banner.png").default} alt="city"/>
+      <Row xs={2} md={4} xl={5} 
+        className="flex-column popularCities g-3 section"
+      >
+        {
+          popularCities.map((item, index) => (
+            <Col>
+              <Card className={`shadow ${index % 3 === 0 ? 'p-3' : 'p-2'}`}>
+                <Ratio aspectRatio={index % 3 === 0 ? '3x4' : '5x3'}>
+                  <img src={require("./static/banner.png").default} alt="city" />
                 </Ratio>
-                <Card.ImgOverlay>
-                  123
+                <Card.ImgOverlay 
+                  className="d-flex flex-column align-items-center justify-content-center"
+                >
+                  <MapM className="mb-2"/>
+                  <Card.Title className="text-light fs-2">{item.city}</Card.Title>
                 </Card.ImgOverlay>
-            </Card>
-        </Col>
-        <Col className="d-flex flex-column justify-content-between">
-            <Card className="shadow p-2">
-                <Ratio aspectRatio="5x3">
-                  <img src={require("./static/banner.png").default} alt="city"/>
-                </Ratio>
-                <Card.ImgOverlay>
-                  123
-                </Card.ImgOverlay>
-            </Card>
-            <Card className="shadow p-2">
-                <Ratio aspectRatio="5x3">
-                  <img src={require("./static/banner.png").default} alt="city"/>
-                </Ratio>
-                <Card.ImgOverlay>
-                  123
-                </Card.ImgOverlay>
-            </Card>
-        </Col>
+              </Card>
+            </Col>
+          ))
+        }
       </Row>
     </>
-  )
+  );
 }
 
 export default function Home() {
   return (
     <>
-      <Banner img='Home/static/banner.png' alt="banner"/>
+      <Banner img="Home/static/banner.png" alt="banner" />
       <Container>
-        <Location/>
-        <ActivityList/>
-        <CardList icon='square' title='熱門餐飲'/>
+        <Location />
+        <ActivityList />
+        <CardList icon="square" title="熱門餐飲" />
       </Container>
     </>
-  )
+  );
 }
