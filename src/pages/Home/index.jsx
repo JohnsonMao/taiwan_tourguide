80bab83444c156640bead7f85f64b4bfa4b14b7d
JohnsonMao <86179381+JohnsonMao@users.noreply.github.com>
2021-11-04 22:41:39 +0800

    add route pathname

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Container, Card, Ratio, Button } from "react-bootstrap";
 import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
@@ -22,70 +22,87 @@ function CityList(props) {
   const prevRef = useRef(null);
   const nextRef = useRef(null);
 
+  const handleCity = (e) => {
+    console.log(e.target.dataset.city)
+  }
+
   /* `$popularCities` */
   const popularCities = [
     {
-      city: "台東",
+      cityName: "台東",
+      city: 'TaitungCounty',
       mobile: true,
       img: "https://images.unsplash.com/photo-1584672534914-50ac33ca00f7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "台北",
+      cityName: "台北",
+      city: 'Taipei',
       mobile: true,
       img: "https://images.unsplash.com/photo-1621415814107-4595c7b2b4fe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "台中",
+      cityName: "台中",
+      city: 'Taichung',
       mobile: true,
       img: "https://images.unsplash.com/photo-1614518921956-3dc707dbc218?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "台南",
+      cityName: "台南",
+      city: 'Tainan',
       mobile: true,
       img: "https://images.unsplash.com/photo-1582557881844-12b774f6abb6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "高雄",
+      cityName: "高雄",
+      city: 'Kaohsiung',
       mobile: true,
       img: "https://images.unsplash.com/photo-1552903346-3fef7ff1b4a0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "花蓮",
+      cityName: "花蓮",
+      city: 'HualienCounty',
       mobile: true,
       img: "https://images.unsplash.com/photo-1624650727156-dad96f051375?ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjF8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "宜蘭",
+      cityName: "宜蘭",
+      city: 'YilanCounty',
       mobile: false,
       img: "https://images.unsplash.com/photo-1629372282451-74fba8263845?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "嘉義",
+      cityName: "嘉義",
+      city: 'Chiayi',
       mobile: false,
       img: "https://images.unsplash.com/photo-1586753080433-3b98668a4085?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "南投",
+      cityName: "南投",
+      city: 'NantouCounty',
       mobile: false,
       img: "https://images.unsplash.com/photo-1635441629083-de8117bde098?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "屏東",
+      cityName: "屏東",
+      city: 'PingtungCounty',
       mobile: false,
       img: "https://images.unsplash.com/photo-1609147110643-963090d85ff5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "苗栗",
+      cityName: "苗栗",
+      city: 'MiaoliCounty',
       mobile: false,
       img: "https://images.unsplash.com/photo-1542976813-986a8fccbd1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "新竹",
+      cityName: "新竹",
+      city: 'HsinchuCounty',
       mobile: false,
       img: "https://images.unsplash.com/photo-1592912925002-02623714abfb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
     {
-      city: "外島",
+      cityName: "澎湖",
+      city: 'PenghuCounty',
       mobile: false,
       img: "https://images.unsplash.com/photo-1578028076641-ef1d08387c14?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=10",
     },
@@ -94,15 +111,15 @@ function CityList(props) {
   /* `$SwiperSlide` */
   const CitySwiperSlide = popularCities.map((item, index) => (
     <SwiperSlide key={index} tag="li">
-      <Link to="/scenicspot" onClick={setSelect}>
+      <Link to={`/scenicspot?city=${item.city}`}>
         <Card className={`shadow ${index % 3 === 0 ? "p-3 mt-2" : "p-2"}`}>
           <Ratio aspectRatio={index % 3 === 0 ? "3x4" : "5x3"}>
-            <img src={item.img} alt={item.city} />
+            <img src={item.img} alt={item.cityName} />
           </Ratio>
           <Card.ImgOverlay className="d-flex flex-column align-items-center justify-content-center">
             <MapM className="mb-2" />
             <Card.Title className="text-light fs-2" as="h3">
-              {item.city}
+              {item.cityName}
             </Card.Title>
           </Card.ImgOverlay>
         </Card>
@@ -133,7 +150,7 @@ function CityList(props) {
       swiper.params.navigation.nextEl = nextRef.current;
       swiper.navigation.init();
       swiper.navigation.update();
-    },
+    }
   };
   return (
     <section className="popularCities position-relative">
